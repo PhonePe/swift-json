7fac07d638e36dd3f6494b11ecc00b9ef92b30f1
Vatsal Manot <vatsal.manot@yahoo.com>
2018-06-29 12:57:04 +0530

    Yet again improve error logging and recovery

--- Sources/ErrorLogging.swift
+++ Sources/ErrorLogging.swift
@@ -7,7 +7,54 @@ import Swift
 
 private let debug: Bool = false
 
-public func printIfDebug(_ string: String) {
+extension JSON {
+    internal func toJSONStringForDebugging() -> String {
+        let encoder = JSONEncoder()
+        
+        if #available(iOS 11.0, *) {
+            encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
+        } else {
+            encoder.outputFormatting = [.prettyPrinted]
+        }
+        
+        do {
+            let data = try encoder.encode(self)
+            do {
+                return try String(data: data, encoding: .utf8).unwrap()
+            } catch {
+                return "<<corrupted JSON>>"
+            }
+        } catch {
+            return "<<error encoding JSON>>"
+        }
+    }
+}
+
+extension KeyedDecodingContainerProtocol {
+    internal func decodeJSONStringForDebugging(forKey key: Key) -> String {
+        return (try? decode(JSON.self, forKey: key).toJSONStringForDebugging()) ?? "<<error decoding JSON for debugging>>"
+    }
+}
+
+extension SingleValueDecodingContainer {
+    internal func decodeJSONStringForDebugging() -> String {
+        return (try? decode(JSON.self).toJSONStringForDebugging()) ?? "<<error decoding JSON for debugging>>"
+    }
+}
+
+extension UnkeyedDecodingContainer {
+    internal mutating func decodeJSONStringForDebugging() -> String {
+        return (try? decode(JSON.self).toJSONStringForDebugging()) ?? "<<error decoding JSON for debugging>>"
+    }
+}
+
+extension JSON: CustomDebugStringConvertible {
+    public var debugDescription: String {
+        return toJSONStringForDebugging()
+    }
+}
+
+internal func printIfDebug(_ string: String) {
     if debug {
         print(string)
     }
@@ -16,16 +63,16 @@ public func printIfDebug(_ string: String) {
 internal func logDecodeError<Container: KeyedDecodingContainerProtocol, T: Decodable>(container: Container, type: T.Type, key: Container.Key, error: Error) {
     
     if case DecodingError.typeMismatch(_, _) = error {
-        let json = (try? container.decode(JSON.self, forKey: key).toJSONString()).flatMap({ $0 }) ?? "<<error decoding JSON>>"
-        printIfDebug("Decoding Error: Expected \(T.self) for \(key.stringValue), found:\n\(json)")
+        let json = container.decodeJSONStringForDebugging(forKey: key)
+        printIfDebug("Error while decoding \(Container.self): expected \(T.self) for \(key.stringValue) but found:\n\(json)")
     }
 }
 
 internal func logDecodeError<Container: SingleValueDecodingContainer, T: Decodable>(container: Container, type: T.Type, error: Error) {
     
     if case DecodingError.typeMismatch(_, _) = error {
-        let json = (try? container.decode(JSON.self).toJSONString()).flatMap({ $0 }) ?? "<<error decoding JSON>>"
-        printIfDebug("Decoding Error: Expected \(T.self), found:\n\(json)")
+        let json = container.decodeJSONStringForDebugging()
+        printIfDebug("Error while decoding \(Container.self): expected \(T.self) but found:\n\(json)")
     }
 }
 
